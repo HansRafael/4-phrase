553be5260820f9aad2af33b0984369c5191c1807
Hans Ruebenich <hans.rafael2000@gmail.com>
2023-11-16 10:47:05 -0300

    adding error handling

--- app/webscraping/oxford.py
+++ app/webscraping/oxford.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 
 from app.domain.dictionary import Dictionary, MeaningWord, Sources
 
+from app.configs.logger import get_logger
+
+logger = get_logger(__name__)
 
 class OxfordScraping():
     def __init__(self, web_response, url) -> Dictionary:
@@ -9,18 +12,21 @@ class OxfordScraping():
         self.dictionary = Dictionary(source=Sources.OXFORD_DICTIONARY.value, source_url=url)
 
     def get_definition_from_oxford_dictionary(self):
-        senses = self.web_page.find_all('li', class_='sense')
-        
-        for s in senses:
-            meaning = MeaningWord()
-            definition = s.find('span', class_='def').text
-            meaning.definition = definition
+        try:
+            senses = self.web_page.find_all('li', class_='sense')
+            
+            for s in senses:
+                meaning = MeaningWord()
+                definition = s.find('span', class_='def').text
+                meaning.definition = definition
 
-            examples = s.find_all('ul', class_='examples')
-            for e in examples:
-                for ex in e.find_all('li'):
-                    meaning.examples.append(ex.text)
-            self.dictionary.meaning.append(meaning)
-        
+                examples = s.find_all('ul', class_='examples')
+                for e in examples:
+                    for ex in e.find_all('li'):
+                        meaning.examples.append(ex.text)
+                self.dictionary.meaning.append(meaning)
+        except Exception as ex:
+            logger.warning(ex)
+
         return self.dictionary
     
